6ee21bd9325cb0acfb72c44b612b444e988cedef
zahrafarrokhi <zahrafarrokhi2017@gmail.com>
2021-02-14 00:58:30 +0330

    added files mod_users

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask
-from mod_admin import admin
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Development
+
+
 app=Flask(__name__)
-app.register_blueprint(admin)
+app.config.from_object(Development)
+
+# db
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
     return "hello world!"
+
+
+# blueprint
+from mod_admin import admin
+from mod_users import users
+
+app.register_blueprint(admin)
+app.register_blueprint(users)
