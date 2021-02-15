8ebc00bfcba554a7bd67865bb7d4a3117b1ea691
zahrafarrokhi <zahrafarrokhi2017@gmail.com>
2021-02-15 22:41:01 +0330

    added all

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,4 +1,6 @@
-from flask import session
+from flask import session, render_template, request, abort
+from mod_users.forms import LoginForm
+from mod_users.models import User
 from . import admin
 
 
@@ -6,12 +8,27 @@ from . import admin
 def index():
     return "Hello from admin Index"
 
-
-
-@admin.route('/login/')
+@admin.route('/login/', methods=['GET', 'POST'])
 def login():
-    session['name'] = 'Zahra'
-    # session.clear()
-    # print(session.get('name'))
-    print(session)
-    return "1"
+    form = LoginForm(request.form)
+    if request.method == 'POST':
+        # print(f"form is validated!{form.validate_on_submit()}")
+        # form is validated!True
+        if not form.validate_on_submit():
+            abort(400)
+        user = User.query.filter(User.email.ilike(f'{form.email.data}')).first()
+        # print(user)
+        # id user in db 
+        # <User 1>
+        if not user:
+            return "Incorrent Credentials", 400
+        if not user.check_password(form.password.data):
+            return "Incorrent Credentials", 400
+        session['email'] = user.email
+        session['user_id'] = user.id
+        return "Logged in successfully" 
+    print(session) 
+    # <SecureCookieSession {'csrf_token': 'd36f80594222731588967b204cd4b876d5e0df38', 'email': 'zahrafarrokhi2017@gamil.com', 'name': 'Zahra', 'user_id': 1}>  
+    if session.get('email') is not None:
+        return "You are already logged in"
+    return render_template('admin/login.html', form=form)
